697ba43fb509cb085f83cdfa6c0e6d71a152cf3c
Prithvijit Guha <guhap01@heiway.net>
2026-06-22 18:06:38 +0530

    Updated commands for git and nvim lock

--- private_dot_config/nvim/lua/plugins/extra_plugins.lua
+++ private_dot_config/nvim/lua/plugins/extra_plugins.lua
@@ -34,4 +34,37 @@ return {
       { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Got to the right pane" },
     },
   },
+  {
+    "PedramNavid/dbtpal",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    ft = {
+      "sql",
+      "md",
+      "yaml",
+    },
+    keys = {
+      { "<leader>drf", "<cmd>DbtRun<cr>" },
+      { "<leader>drp", "<cmd>DbtRunAll<cr>" },
+      { "<leader>dtf", "<cmd>DbtTest<cr>" },
+      { "<leader>dm", "<cmd>lua require('dbtpal.telescope').dbt_picker()<cr>" },
+    },
+    config = function()
+      require("dbtpal").setup({
+        path_to_dbt = "dbt",
+        path_to_dbt_project = "",
+        path_to_dbt_profiles_dir = vim.fn.expand("~/.dbt"),
+        include_profiles_dir = true,
+        include_project_dir = true,
+        include_log_level = true,
+        extended_path_search = true,
+        protect_compiled_files = true,
+        pre_cmd_args = {},
+        post_cmd_args = {},
+      })
+      require("telescope").load_extension("dbtpal")
+    end,
+  },
 }
