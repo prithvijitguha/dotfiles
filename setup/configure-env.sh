eacd96d659a35c4d0c7a72baac1f4d77a64f6d3a
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-08-02 10:02:20 +0530

    Updated install scripts and packages required

--- setup/configure-env.sh
+++ setup/configure-env.sh
@@ -2,125 +2,63 @@
 
 set -e
 
-# Homebrew
-if [ ! -d "/home/linuxbrew/.linuxbrew" ]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt update
+
+sudo apt install -y git
+sudo apt install -y curl
+sudo apt install -y wget
+sudo apt install -y zsh
+sudo apt install -y tmux
+sudo apt install -y vim
+sudo apt install -y htop
+sudo apt install -y unzip
+sudo apt install -y build-essential
+sudo apt install -y ca-certificates
+sudo apt install -y apt-transport-https
+sudo apt install -y gpg
+sudo apt install -y flatpak
+sudo apt install -y fonts-noto-color-emoji
+sudo apt install -y flameshot
+sudo apt install -y fd-find
+sudo apt install -y fzf
+sudo apt install -y gpg
+sudo apt install -y pipx
+sudo apt install -y gir1.2-gnomedesktop-3.0
+sudo apt install -y gir1.2-gnomedesktop-4.0
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install -y steam
+# Install eza
+if ! command -v eza >/dev/null 2>&1; then
+  sudo mkdir -p /etc/apt/keyrings
+
+  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc |
+    sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+
+  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" |
+    sudo tee /etc/apt/sources.list.d/gierens.list
+
+  sudo chmod 644 \
+    /etc/apt/keyrings/gierens.gpg \
+    /etc/apt/sources.list.d/gierens.list
 fi
+# Stremio
+sudo apt update
+sudo apt install -y flatpak
 
-# Chezmoi install
-if ! command -v chezmoi >/dev/null 2>&1; then
-  brew install chezmoi
-fi
+# Install signal
+# 1. Install our official public software signing key:
+curl https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor >signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg >/dev/null
 
-# Download and install Node.js:
-if ! command -v node@24 >/dev/null 2>&1; then
-  brew install node@24
-  # Verify the Node.js version:
-  node -v # Should print "v24.18.1".
-  # Verify npm version:
-  npm -v # Should print "11.16.0".
-fi
+# 2. Add our repository to your list of repositories:
+curl -o signal-desktop.sources https://updates.signal.org/static/desktop/apt/signal-desktop.sources
+cat signal-desktop.sources | sudo tee /etc/apt/sources.list.d/signal-desktop.sources >/dev/null
 
-if ! npm list -g typescript >/dev/null 2>&1; then
-  npm install -g typescript
-fi
+# 3. Update your package database and install Signal:
+sudo apt update && sudo apt install signal-desktop
 
-# UV
-if [ ! -f "$HOME/.local/bin/uv" ]; then
-  curl -LsSf https://astral.sh/uv/install.sh | sh
-fi
+flatpak remote-add --if-not-exists flathub \
+  https://flathub.org/repo/flathub.flatpakrepo
 
-# RUSTUP
-if [ ! -f "$HOME/.cargo/bin/rustup" ]; then
-  curl https://sh.rustup.rs -sSf | sh -s -- -y
-fi
-
-# OH MY ZSH
-if [ ! -d "$HOME/.oh-my-zsh/.git" ]; then
-  git clone --depth=1 \
-    https://github.com/ohmyzsh/ohmyzsh.git \
-    "$HOME/.oh-my-zsh"
-fi
-
-# POWERLEVEL10K
-if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k/.git" ]; then
-  git clone --depth=1 \
-    https://github.com/romkatv/powerlevel10k.git \
-    "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
-fi
-
-# JETBRAINS MONO NERD FONT
-mkdir -p "$HOME/.local/share/fonts"
-
-# JETBRAINS MONO NERD FONT
-if [ ! -d "$HOME/.local/share/fonts/JetBrainsMono" ]; then
-  mkdir -p "$HOME/.local/share/fonts/JetBrainsMono"
-
-  wget \
-    -O /tmp/JetBrainsMono.zip \
-    https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
-
-  unzip -o /tmp/JetBrainsMono.zip \
-    -d "$HOME/.local/share/fonts/JetBrainsMono"
-
-  fc-cache -fv
-fi
-
-# PAPIRUS
-if ! dpkg -s papirus-icon-theme >/dev/null 2>&1; then
-  sudo apt update
-  sudo apt install -y papirus-icon-theme
-fi
-
-if ! dpkg -s gnome-sushi >/dev/null 2>&1; then
-  sudo apt update
-  sudo apt install -y gnome-sushi
-fi
-
-# Install zsh plugins
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/.git" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions \
-    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-fi
-
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/.git" ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-fi
-
-# Install ghostty
-if ! command -v ghostty >/dev/null 2>&1; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
-fi
-
-# Install bat
-if ! command -v bat >/dev/null 2>&1 && ! command -v batcat >/dev/null 2>&1; then
-  git clone https://github.com/bats-core/bats-core.git /tmp/bats-core
-  cd /tmp/bats-core
-  ./install.sh /usr/local
-fi
-
-if ! command -v nb >/dev/null 2>&1; then
-  npm install -g nb.sh
-fi
-
-# Google Chrome
-if ! command -v google-chrome >/dev/null 2>&1; then
-  wget -q -O /tmp/google-chrome.deb \
-    https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-  sudo apt install -y /tmp/google-chrome.deb
-
-  rm -f /tmp/google-chrome.deb
-fi
-
-# Hunk CLI
-if ! command -v hunk >/dev/null 2>&1; then
-  brew install hunk
-  # Install hunk for page diff for git
-  git config --global core.pager "hunk pager"
-fi
-
-# Make zsh default shell
-chsh -s $(which zsh)
+flatpak install -y flathub com.stremio.Stremio
--- setup/install-apt-packages.sh
+++ setup/install-apt-packages.sh
@@ -24,6 +24,7 @@ sudo apt install -y fzf
 sudo apt install -y gpg
 sudo apt install -y pipx
 sudo apt install -y gir1.2-gnomedesktop-4.0
+sudo apt install -y gir1.2-gnomedesktop-3.0
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install -y steam
