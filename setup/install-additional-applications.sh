6890beb8c63536a537b4627e66a4c86c67ef3c32
Prithvijit Guha <71138854+prithvijitguha@users.noreply.github.com>
2026-08-03 12:05:06 +0530

    Switched package from brew to node

--- setup/install-additional-applications.sh
+++ setup/install-additional-applications.sh
@@ -15,7 +15,12 @@ fi
 
 # Download and install Node.js:
 if ! command -v node >/dev/null 2>&1; then
-  brew install node@24
+  # Download and install nvm:
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | bash
+  # in lieu of restarting the shell
+  \. "$HOME/.nvm/nvm.sh"
+  # Download and install Node.js:
+  nvm install 24
   # Verify the Node.js version:
   node -v # Should print "v24.18.1".
   # Verify npm version:
